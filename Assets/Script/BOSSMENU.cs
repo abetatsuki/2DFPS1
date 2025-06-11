dd64d04265d77ded8df2a962e256869aca4d7d7e
kenkatou354 <kenkatou354@gmail.com>
2025-06-12 00:20:52 +0900

    メニュー画面を追加

--- Assets/Script/BOSSMENU.cs
+++ Assets/Script/BOSSMENU.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro; // TextMeshProを使う場合
+using UnityEngine.SceneManagement;
 public class BOSSMENU : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text[] menuItems; // InspectorでTextを配列でセット
+    private int selectedIndex = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 上キー
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectedIndex--;
+            if (selectedIndex < 0) selectedIndex = menuItems.Length - 1;
+            UpdateMenu();
+        }
+
+        // 下キー
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectedIndex++;
+            if (selectedIndex >= menuItems.Length) selectedIndex = 0;
+            UpdateMenu();
+        }
+
+        // 決定キー
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log(menuItems[selectedIndex].text + " が選択されました");
+
+            if (menuItems[selectedIndex].text == "END")
+            {
+                Application.Quit(); // ビルド時にアプリ終了
+            }
+            if (menuItems[selectedIndex].text == "START")
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    void UpdateMenu()
+    {
+        // 選択中の項目だけ色変更
+        for (int i = 0; i < menuItems.Length; i++)
+        {
+            if (i == selectedIndex)
+                menuItems[i].color = Color.yellow; // 選択中
+            else
+                menuItems[i].color = Color.white; // 未選択
+        }
     }
 }
