b22e0f8da4253509be7bd5c2544f7d5b55a7ff92
kenkatou354 <kenkatou354@gmail.com>
2025-06-16 14:08:17 +0900

    enemyscale

--- Assets/Script/EnemyGeneretor.cs
+++ Assets/Script/EnemyGeneretor.cs
@@ -7,7 +7,7 @@ public class EnemyGeneretor : MonoBehaviour
     public GameObject enemyPrefab;         // 敵のプレハブ
     public float spawnInterval = 2f;       // 出現間隔（秒）
     public float xRange = 8f;              // 出現範囲（X座標の幅）
-
+    public Transform EnemyScale;
     void Start()
     {
         InvokeRepeating("SpawnEnemy", 1f, spawnInterval);
@@ -17,6 +17,7 @@ public class EnemyGeneretor : MonoBehaviour
     {
         float x = Random.Range(-xRange, xRange);
         Vector3 spawnPosition = new Vector3(x, transform.position.y, 0);
+
         Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
     }
 }
