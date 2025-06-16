6e436f6bab6b53d6d6f484cf19db7a3631065c2f
kenkatou354 <kenkatou354@gmail.com>
2025-06-16 14:12:34 +0900

    randam scale変数

--- Assets/Script/EnemyGeneretor.cs
+++ Assets/Script/EnemyGeneretor.cs
@@ -8,6 +8,7 @@ public class EnemyGeneretor : MonoBehaviour
     public float spawnInterval = 2f;       // 出現間隔（秒）
     public float xRange = 8f;              // 出現範囲（X座標の幅）
     public Transform EnemyScale;
+
     void Start()
     {
         InvokeRepeating("SpawnEnemy", 1f, spawnInterval);
@@ -15,9 +16,11 @@ public class EnemyGeneretor : MonoBehaviour
 
     void SpawnEnemy()
     {
+        // 1～3の整数の中からランダムに選ぶ
+        int randomScale = Random.Range(1, 4); // 4は含まれないので1,2,3のどれかになる
         float x = Random.Range(-xRange, xRange);
         Vector3 spawnPosition = new Vector3(x, transform.position.y, 0);
-
+        EnemyScale.localScale = new Vector3(1f, 1f, 1f);
         Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
     }
 }
